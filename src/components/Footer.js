0d4413722ec2d4c3cb20818f3a815de75f7b36ad
Marta Frąckiewicz <frackiewiczmarta2@gmail.com>
2021-05-06 21:21:03 +0200

    change footer description

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,7 +7,7 @@ const Footer = () => {
     return (
         <div className="container footer-container">
             <footer className="footer">
-                <small className="footer-copy">Copyright by Coders Lab</small>
+                <small className="footer-copy">Project by Coders Lab</small>
                 <div className="footer-social">
                     <a className="link" href="https://www.facebook.com/CodersLabSzkolaIT/"><img src={facebook}></img></a>
                     <a className="link" href="https://www.instagram.com/coders.lab/?hl=pl"><img src={instagram}></img></a>
